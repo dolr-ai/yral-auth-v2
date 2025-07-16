31d8519382f92d86cfd282429412060c4c370f1a
rupansh <rupanshsekar@hotmail.com>
2025-07-16 15:43:02 +0530

    chore(flake): use fenix

--- flake.nix
+++ flake.nix
@@ -2,17 +2,21 @@
     description = "A basic flake providing a shell with rustup";
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-        rust-overlay.url = "github:oxalica/rust-overlay";
         flake-utils.url = "github:numtide/flake-utils";
+        fenix = {
+          url = "github:nix-community/fenix";
+          inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = {self, nixpkgs, flake-utils, rust-overlay}: 
+    outputs = {self, nixpkgs, flake-utils, fenix}: 
         flake-utils.lib.eachDefaultSystem (system: 
             let 
-                overlays = [ (import rust-overlay) ];
+                overlays = [ fenix.overlays.default ];
                 pkgs = import nixpkgs {
                     inherit system overlays;
-                };    
+                };
+                rustTc = fenix.packages.${system}.fromToolchainFile { dir = ./.; sha256 = "sha256-Qxt8XAuaUR2OMdKbN4u8dBJOhSHxS+uS06Wl9+flVEk="; };
                 in
                 {
                     devShells.default = pkgs.mkShell {
@@ -22,7 +26,7 @@
                             nodejs_22
                             cargo-leptos
                             tailwindcss_4
-                            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+                            rustTc
                         ];
                     };
                 }
