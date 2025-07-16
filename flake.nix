81c325e89999b528dad89a8a85506ec590aa85ee
rupansh <rupanshsekar@hotmail.com>
2025-07-16 14:56:37 +0530

    chore(flake): use rust-overlay

--- flake.nix
+++ flake.nix
@@ -2,14 +2,16 @@
     description = "A basic flake providing a shell with rustup";
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+        rust-overlay.url = "github:oxalica/rust-overlay";
         flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = {self, nixpkgs, flake-utils}: 
+    outputs = {self, nixpkgs, flake-utils, rust-overlay}: 
         flake-utils.lib.eachDefaultSystem (system: 
             let 
+                overlays = [ (import rust-overlay) ];
                 pkgs = import nixpkgs {
-                    inherit system;
+                    inherit system overlays;
                 };    
                 in
                 {
@@ -18,9 +20,9 @@
                             binaryen
                             leptosfmt
                             nodejs_22
-                            rustup
                             cargo-leptos
                             tailwindcss_4
+                            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
                         ];
                     };
                 }
