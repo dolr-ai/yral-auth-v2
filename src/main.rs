d75ed778272e6fa377820a3d8bcc4f1a2d75c689
Ravi Sawlani <ravisawlani04@gmail.com>
2026-02-26 22:26:35 +0530

    setup rate limiting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{num::NonZeroU32, sync::Arc};
 
 use axum::{
     body::Body as AxumBody,
@@ -12,6 +12,7 @@ use leptos::{config::get_configuration, logging::log, prelude::provide_context};
 use leptos_axum::{generate_route_list, handle_server_fns_with_context, LeptosRoutes};
 use sentry_tower::{NewSentryLayer, SentryHttpLayer};
 use tower::ServiceBuilder;
+use tower_governor::{GovernorLayer, governor::{Governor, GovernorConfig, GovernorConfigBuilder}, key_extractor::SmartIpKeyExtractor};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use yral_auth_v2::{
@@ -116,12 +117,22 @@ async fn main() {
         .layer(NewSentryLayer::new_from_top())
         .layer(SentryHttpLayer::with_transaction());
 
+
+let governor_conf = GovernorConfigBuilder::default()
+    .per_second(5)
+    .burst_size(10)
+    .key_extractor(SmartIpKeyExtractor)
+    .finish()
+    .unwrap();
+
+
     let app = Router::new()
         .route(
             "/api/{*fn_name}",
             get(server_fn_handler).post(server_fn_handler),
         )
         .leptos_routes_with_handler(routes, get(leptos_routes_handler))
+        .layer(GovernorLayer::new(governor_conf))
         .fallback(leptos_axum::file_and_error_handler::<ServerState, _>(shell))
         .with_state(app_state)
         .merge(server_routes(ctx))
